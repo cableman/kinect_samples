f6ea289480505c6e100096b5377d20c162f47ef5
Jesper Kristensen <cableman0408@gmail.com>
2012-02-11 17:39:16 +0100

    Added depth image test

--- KinectSample/MainWindow.xaml.cs
+++ KinectSample/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace KinectSample
         {
             // Stop old senson   
             KinectSensor oldSensor = (KinectSensor)e.OldValue;
-            KinectStop(oldSensor);
+            kinectStop(oldSensor);
 
             // Get the new sensor
             KinectSensor newSensor = (KinectSensor)e.NewValue;
@@ -57,29 +57,23 @@ namespace KinectSample
             {
                 kinectSensorChooser.AppConflictOccurred();
             }
+
+            lblCurrentAngle.Content = kinectSensorChooser.Kinect.ElevationAngle.ToString();
+            slider1.Value = kinectSensorChooser.Kinect.ElevationAngle;
         }
 
-        void newSensort_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        private void newSensort_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
-            using(ColorImageFrame colorFrame = e.OpenColorImageFrame()) 
-            {
-                if (colorFrame == null) { return; }
-                byte[] pixels = new Byte[colorFrame.PixelDataLength];
-                colorFrame.CopyPixelDataTo(pixels);
-
-                WriteableBitmap wbitmap = new WriteableBitmap(colorFrame.Width, colorFrame.Height, 96, 96, PixelFormats.Bgr32, null);
-                int stride = colorFrame.Width * 4;
-                wbitmap.WritePixels(new Int32Rect(0, 0, wbitmap.PixelWidth, wbitmap.PixelHeight), pixels, stride, 0);
-                colorImage.Source = wbitmap;
-            }
+            kinectUpdateColorImage(e.OpenColorImageFrame());
+            kinectUpdateDepthImage(e.OpenDepthImageFrame());            
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-
+            kinectStop(kinectSensorChooser.Kinect);
         }
 
-        private void KinectStop(KinectSensor sensor)
+        private void kinectStop(KinectSensor sensor)
         {
             if (sensor != null)
             {
@@ -93,5 +87,151 @@ namespace KinectSample
                 }
             }
         }
+
+        # region Depht image processing
+
+        const float MaxDepthDistance = 4095; // max value returned
+        const float MinDepthDistance = 850; // min value returned
+        const float MaxDepthDistanceOffset = MaxDepthDistance - MinDepthDistance;
+
+        private static WriteableBitmap wbitmapDepth = new WriteableBitmap(640, 480, 96, 96, PixelFormats.Bgr32, null);
+        private void kinectUpdateDepthImage(DepthImageFrame depthFrame)
+        {
+            if (depthFrame == null) { return; }
+
+            byte[] pixels = GenerateColoredBytes(depthFrame);
+
+            // Update the image
+            int stride = depthFrame.Width * 4; // (B,G,R,Empty)
+            wbitmapDepth.WritePixels(new Int32Rect(0, 0, wbitmapDepth.PixelWidth, wbitmapDepth.PixelHeight), pixels, stride, 0);
+            depthImage.Source = wbitmapDepth;
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+            // Get the raw data from kinect with the depth for every pixel
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData); 
+
+            //use depthFrame to create the image to display on-screen
+            //depthFrame contains color information for all pixels in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            //Bgr32  - Blue, Green, Red, empty byte
+            //Bgra32 - Blue, Green, Red, transparency 
+            //You must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
+
+            //hardcoded locations to Blue, Green, Red (BGR) index positions       
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            
+            //loop through all distances
+            //pick a RGB color based on distance
+            for (int depthIndex = 0, colorIndex = 0; 
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length; 
+                depthIndex++, colorIndex += 4)
+            {
+                //get the player (requires skeleton tracking enabled for values)
+                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+
+                //gets the depth value
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                //.9M or 2.95'
+                if (depth <= 900)
+                {
+                    //we are very close
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+
+                }
+                // .9M - 2M or 2.95' - 6.56'
+                else if (depth > 900 && depth < 2000)
+                {
+                    //we are a bit further away
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                // 2M+ or 6.56'+
+                else if (depth > 2000)
+                {
+                    //we are the farthest
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 255;
+                }
+
+
+                ////equal coloring for monochromatic histogram
+                //byte intensity = CalculateIntensityFromDepth(depth);
+                //pixels[colorIndex + BlueIndex] = intensity;
+                //pixels[colorIndex + GreenIndex] = intensity;
+                //pixels[colorIndex + RedIndex] = intensity;
+
+
+                //Color all players "gold"
+                if (player > 0)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+
+            }
+          
+
+            return pixels;
+        }
+
+
+        public static byte CalculateIntensityFromDepth(int distance)
+        {
+            // Formula for calculating monochrome intensity for histogram
+            return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0) / (MaxDepthDistanceOffset)));
+        }
+
+        # endregion
+
+        # region Color image processing
+        private static WriteableBitmap wbitmapColor = new WriteableBitmap(640, 480, 96, 96, PixelFormats.Bgr32, null);
+        private void kinectUpdateColorImage(ColorImageFrame colorFrame)
+        {
+            if (colorFrame == null) { return; }
+
+            // Get pixel values
+            byte[] pixels = new Byte[colorFrame.PixelDataLength];
+            colorFrame.CopyPixelDataTo(pixels);
+            
+            // Update the image
+            int stride = colorFrame.Width * 4;
+            wbitmapColor.WritePixels(new Int32Rect(0, 0, wbitmapColor.PixelWidth, wbitmapColor.PixelHeight), pixels, stride, 0);
+            colorImage.Source = wbitmapColor;
+        }
+        # endregion
+
+        # region Tilt handler
+        /**
+         * Handle the tilt events, when slider is moved. 
+         */
+        private void slider1_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            slider1.IsEnabled = false;
+
+            // Set angle to slider value
+            if (kinectSensorChooser.Kinect != null && kinectSensorChooser.Kinect.IsRunning)
+            {
+                kinectSensorChooser.Kinect.ElevationAngle = (int)slider1.Value;
+                lblCurrentAngle.Content = kinectSensorChooser.Kinect.ElevationAngle;
+            }
+
+            System.Threading.Thread.Sleep(new TimeSpan(hours: 0, minutes: 0, seconds: 1));
+            slider1.IsEnabled = true;
+        }
+        #endregion
     }
 }
